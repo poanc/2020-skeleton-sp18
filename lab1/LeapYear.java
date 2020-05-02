ef4880404fe01be8908c42429316ec0c168d4295
JaneLab <janelab@DHCP-22030.iis.sinica.edu.tw>
2020-05-03 07:45:44 +0800

    complete second part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,11 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Joe Chou
  */
 public class LeapYear {
+
+    private static boolean isLeapYear (int year) {
+         return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
